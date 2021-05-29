677b6410f84048b9ede438397bddf4f11aa83406
Héo <84056145+nguyenhieuit@users.noreply.github.com>
2021-05-29 09:09:49 +0700

    Add files via upload

--- Site1.Master.cs
+++ Site1.Master.cs
@@ -4,14 +4,26 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using System.Data.SqlClient;
+using System.Data;
 
 namespace Buoi7
 {
     public partial class Site1 : System.Web.UI.MasterPage
     {
+        string link = @"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=G:\KTTMDT\Buoi7\Buoi7\Buoi7\App_Data\sql_buoi7.mdf;Integrated Security=True";
         protected void Page_Load(object sender, EventArgs e)
         {
-
+            load_dtList();
+        }
+        public void load_dtList()
+        {
+            string sql = "Select *From LoaiHang";
+            SqlDataAdapter da = new SqlDataAdapter(sql, link);
+            DataTable dt = new DataTable();
+            da.Fill(dt);
+            dt_list1.DataSource = dt;
+            this.dt_list1.DataBind();
         }
     }
 }
--- Site1.Master.designer.cs
+++ Site1.Master.designer.cs
@@ -87,6 +87,15 @@ namespace Buoi7
         protected global::System.Web.UI.WebControls.HyperLink HyperLink5;
 
         /// <summary>
+        /// HyperLink7 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.HyperLink HyperLink7;
+
+        /// <summary>
         /// Image2 control.
         /// </summary>
         /// <remarks>
@@ -96,40 +105,13 @@ namespace Buoi7
         protected global::System.Web.UI.WebControls.Image Image2;
 
         /// <summary>
-        /// HyperLink6 control.
+        /// dt_list1 control.
         /// </summary>
         /// <remarks>
         /// Auto-generated field.
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
-        protected global::System.Web.UI.WebControls.HyperLink HyperLink6;
-
-        /// <summary>
-        /// HyperLink10 control.
-        /// </summary>
-        /// <remarks>
-        /// Auto-generated field.
-        /// To modify move field declaration from designer file to code-behind file.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.HyperLink HyperLink10;
-
-        /// <summary>
-        /// HyperLink8 control.
-        /// </summary>
-        /// <remarks>
-        /// Auto-generated field.
-        /// To modify move field declaration from designer file to code-behind file.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.HyperLink HyperLink8;
-
-        /// <summary>
-        /// HyperLink9 control.
-        /// </summary>
-        /// <remarks>
-        /// Auto-generated field.
-        /// To modify move field declaration from designer file to code-behind file.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.HyperLink HyperLink9;
+        protected global::System.Web.UI.WebControls.DataList dt_list1;
 
         /// <summary>
         /// ContentPlaceHolder1 control.
